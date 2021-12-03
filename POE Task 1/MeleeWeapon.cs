b4a7fd272a9b8e5a2e3e03c3322a0a513905dc75
VinnieRepo <91118842+VinnieRepo@users.noreply.github.com>
2021-12-03 19:36:16 +0200

    Started on question 2

---
+++ POE Task 1/MeleeWeapon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POE_Task_1
+{
+    class MeleeWeapon : Weapon
+    {
+        public enum Types 
+        { 
+            dagger, 
+            longsword, 
+        }
+        public override string ToString()
+        {
+            return WeaponString;
+        }
+
+       
+        public MeleeWeapon(int x, int y, Tiletypes Mweapon, int type) : base(x, y, Mweapon)
+        {
+            range = 1;
+            chooseWeapon(type);
+        }
+
+        public void chooseWeapon(int choose)
+        {
+
+            switch (choose)
+            {
+                case 0:
+                    WeaponString = "Dagger";
+                    durability = 10;
+                    damage = 3;
+                    cost = 3;
+                    break;
+
+                case 1:
+                    WeaponString = "Longsword";
+                    durability = 6;
+                    damage = 4;
+                    cost = 5;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+
---
+++ POE Task 1/Weapons.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POE_Task_1
+{
+    abstract class Weapon : Item
+    {
+        protected int damage;
+        public int Damage
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+        protected int range;
+        public int Range
+        {
+            get { return range; }
+            set { range = value; }
+        }
+        protected int durability;
+        public int Durability
+        {
+            get { return durability; }
+            set { durability = value; }
+        }
+        protected int cost;
+        public int Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+        private string weaponString;
+        public string WeaponString
+        {
+            get { return weaponString; }
+            set { weaponString = value; }
+        }
+
+        public Weapon(int x, int y, tile.Tiletypes Weapon) : base(x, y, "W",Tiletypes.Weapon)
+        {
+
+        }
+    }
+}
