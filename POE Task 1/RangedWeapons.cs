389f32d9c368ab55d277e46e294640faab6bf22f
VinnieRepo <91118842+VinnieRepo@users.noreply.github.com>
2021-12-04 17:15:59 +0200

    Halfway trhough question 2, finishing it tonight.

---
+++ POE Task 1/RangedWeapons.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POE_Task_1
+{
+    class RangedWeapons : Weapon
+    {
+        public enum Types { Rifle, Longbow };
+        public RangedWeapons(int x, int y, Tiletypes rweapon, int type) : base(x, y, rweapon)
+        {
+            chooseWeapon(type);
+        }
+
+        public void chooseWeapon(int choose)
+        {
+            switch (choose)
+            {
+                case 0:
+                    WeaponString = "Rifle";
+                    durability = 3;
+                    damage = 5;
+                    range = 3;
+                    cost = 7;
+                    break;
+                case 1:
+                    WeaponString = "Longbow";
+                    durability = 4;
+                    damage = 4;
+                    range = 2;
+                    cost = 6;
+                    break;
+               
+                    
+            }
+        }
+
+       
+    }
+}
+
+
